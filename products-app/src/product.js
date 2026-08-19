72de3ff6c4f0def98c1f686574f4a73d3f95a0c1
Sakda0912 <sakdasurbsaima@gmail.com>
2026-08-19 10:34:28 +0700

    products-app

---
+++ products-app/src/product.js
@@ -0,0 +1,16 @@
+export const products = [
+  { 
+    id: 1, 
+    name: "เสื้อยืด", 
+    price: 299, 
+    category: "เสื้อผ้า",
+    image: "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?w=500&q=80"
+  },
+  { 
+    id: 2, 
+    name: "หูฟัง", 
+    price: 990, 
+    category: "อิเล็กฯ",
+    image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=500&q=80"
+  }
+];
